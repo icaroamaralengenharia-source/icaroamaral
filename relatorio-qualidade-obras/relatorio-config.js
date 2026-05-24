05dea426d9fd072009e8c624f4572cc6531d89d6
icaroamaralengenharia-source <icaroamaralengenharia@gmail.com>
2026-05-24 16:06:35 -0300

    Update relatorio-config.js

--- relatorio-qualidade-obras/relatorio-config.js
+++ relatorio-qualidade-obras/relatorio-config.js
@@ -1,5 +1,5 @@
 window.RELATORIO_QUALIDADE_CONFIG = {
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbw6XaKffCEst44LVyDEjnStdAneUX5BEdLla7P0UtFpyyrEV-QpkTNN4DVrAuWFpLmrxg/exec",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbzmgZcecjYWYMVI2e39iLWHNm9LjmG4SPJiKthXG5N7NEk1oqDnwBN6uBPsBTHLXCrj-w/exec",
   maxInconformidades: 12,
   maxImageWidth: 1600,
   jpegQuality: 0.84
